599f32cb1fe8eb2bff6af40d4d39eceda1ef7f41
jinho7 <jinho7>
2022-11-20 16:30:27 +0900

    first commit

---
+++ app/src/main/java/com/example/footballpredictions/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.footballpredictions
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+
+        // 일정 시간 지연 이후 실행하기 위한 코드
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent= Intent( this, MainActivity::class.java)
+            startActivity(intent)
+
+            // 이전 키를 눌렀을 때 스플래스 스크린 화면으로 이동을 방지하기 위해
+            // 이동한 다음 사용안함으로 finish 처리
+            finish()
+
+        }, 2000) // 시간 1초 이후 실행
+    }
+}
