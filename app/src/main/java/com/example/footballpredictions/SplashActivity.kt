7f9353074051485f3b5fbbe505c2edefaf1e9b92
jinho7 <jinho7>
2022-11-20 18:05:08 +0900

    feat: 디자인

--- app/src/main/java/com/example/footballpredictions/SplashActivity.kt
+++ app/src/main/java/com/example/footballpredictions/SplashActivity.kt
@@ -15,13 +15,13 @@ class SplashActivity : AppCompatActivity() {
         // 일정 시간 지연 이후 실행하기 위한 코드
         Handler(Looper.getMainLooper()).postDelayed({
 
-            val intent= Intent( this, MainActivity::class.java)
+            val intent= Intent( this, StartActivity::class.java)
             startActivity(intent)
 
             // 이전 키를 눌렀을 때 스플래스 스크린 화면으로 이동을 방지하기 위해
             // 이동한 다음 사용안함으로 finish 처리
             finish()
 
-        }, 2000) // 시간 1초 이후 실행
+        }, 1500) // 시간 1.5초 이후 실행
     }
 }
--- app/src/main/java/com/example/footballpredictions/StartActivity.kt
+++ app/src/main/java/com/example/footballpredictions/StartActivity.kt
@@ -1,11 +1,54 @@
 package com.example.footballpredictions
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.text.style.ForegroundColorSpan as ForegroundColorSpan1
+
 
 class StartActivity : AppCompatActivity() {
+
+    // TextView 변수
+    lateinit var textView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+
+        val btn = findViewById<Button>(R.id.click_bt) as Button
+
+        btn.setOnClickListener {
+            val intent = Intent(this@StartActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        // 1. TextView 참조
+        textView = findViewById(R.id.start_text)
+        val Qator_color = resources.getColor(R.color.Qatar)
+
+        // 2. String 문자열 데이터 취득
+        val textData: String = textView.text.toString()
+
+        // 3. SpannableStringBuilder 타입으로 변환
+        val builder = SpannableStringBuilder(textData)
+
+        // index=4에 해당하는 문자열(4)에 빨간색 적용
+        val colorChange = ForegroundColorSpan1(Qator_color)
+        builder.setSpan(colorChange, 4, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        // 5. TextView에 적용
+        textView.text = builder
+
+
+
+
     }
 }
